7377083659026d75a16f4b660f9e4e135889e9e5
Raiker82 <ikeramores2005@gmail.com>
2025-03-21 14:29:24 +0100

    html , js y css Examen resultado final

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,1 +1,26 @@
-//Escribe aquí tu código JavaScript
+document.getElementById("oscuro").onclick = function(){
+    document.body.classList.toggle("oscuro");
+}
+
+let formulario = document.getElementById("pescaitoform");
+formulario.onsubmit = function(e){
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("email").value;
+    let direccion = document.getElementById("direccion").value;
+    let telefono = document.getElementById("telefono").value;
+    if(nombre.length == 0 || !correo.includes("@") || direccion.length < 18 || telefono.length == 9){
+        e.preventDefault();
+    }
+    let respuesta = confirm("¿Quieres confirmar tu pedido?");
+    console.log(respuesta);
+    alert("gracias por enviar tu pedido," + nombre);
+    if(!respuesta){
+        e.preventDefault();
+    }
+}
+let contador = document.getElementById("instrucciones") = function(){
+document.getElementById("contador").oninput("contador").body.classList.style;
+}
+
+
+
